2f36fcec8a6513400e0dab9f792fb56fc24ed3e2
jesnaCoddle <jesna.j@coddletech.com>
2026-04-06 16:18:33 +0530

    ProductCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { getProducts } from '../data/products'
 import { Link } from 'react-router-dom';
+import ProductCard from '../components/ProductCard';
 export const Home = () => {
   const products = getProducts();
 
@@ -15,17 +16,7 @@ export const Home = () => {
         <h2 className="page-title">Our Products</h2>
         <div className="product-grid">
           {products.map((product) => (
-            <div className='product-card' key={product.id}>
-              <img src={product.image} alt={product.name} className='product-card-image' />
-              <div className='product-card-content'>
-                <h3 className='product-card-name'>{product.name}</h3>
-                <p className='product-card-price'>${product.price}</p>
-                <div className='product-card-actions'>
-                  <Link className='btn btn-secondary'>View Details</Link>
-                  <button className='btn btn-primary'>Add to Cart</button>
-                </div>
-              </div>
-            </div>
+            <ProductCard key={product.id} product={product} />
           ))}
 
         </div>
